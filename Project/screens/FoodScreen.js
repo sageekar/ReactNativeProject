d2a59779a5ac711083615d645907d4ee19de4e59
Mika <81681242+maykeul27@users.noreply.github.com>
2023-06-07 11:55:14 +0200

    Started FoodScreen

--- Project/screens/FoodScreen.js
+++ Project/screens/FoodScreen.js
@@ -1,12 +1,54 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, TextInput, Button, FlatList, Text, StyleSheet } from 'react-native';
+import axios from 'axios';
 
-function FoodScreen() {
-    return (
-      <View>
-        <Text>Details Screen</Text>
-      </View>
-    );
-  }
+const FoodScreen = () => {
+  const [query, setQuery] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
 
-  export default FoodScreen;
+  const handleSearch = async () => {
+    const appId = 'YOUR_APPLICATION_ID';
+    const appKey = 'YOUR_APPLICATION_KEY';
+
+    try {
+      const response = await axios.get(
+        `https://api.edamam.com/api/food-database/v2/parser?ingr=${query}&app_id=${appId}&app_key=${appKey}`
+      );
+      setSearchResults(response.data.hints);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.searchInput}
+        placeholder="Search food..."
+        onChangeText={setQuery}
+      />
+      <Button title="Search" onPress={handleSearch} />
+      <FlatList
+        data={searchResults}
+        renderItem={({ item }) => <Text>{item.food.label}</Text>}
+        keyExtractor={(item) => item.food.foodId}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+  searchInput: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 8,
+    paddingHorizontal: 8,
+  },
+});
+
+export default FoodScreen;
