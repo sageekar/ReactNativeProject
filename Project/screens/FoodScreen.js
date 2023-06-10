6100ed8fe675b8eacbc4572f53fdb96ba60bf48c
sageekar <ragavi.sageekaran@edu.esiee.fr>
2023-06-10 23:30:59 +0200

    ajout des informations relatives aux aliments et modal

--- Project/screens/FoodScreen.js
+++ Project/screens/FoodScreen.js
@@ -1,36 +1,107 @@
-import React, { useState } from 'react';
-import { View, TextInput, Button, FlatList, Text, StyleSheet } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
 import axios from 'axios';
+import React, { useState } from 'react';
+import {
+  View,
+  TextInput,
+  Button,
+  FlatList,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Modal,
+} from 'react-native';
+
 
 const FoodScreen = () => {
   const [query, setQuery] = useState('');
   const [searchResults, setSearchResults] = useState([]);
+  const [error, setError] = useState(null);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [Quantity, setQuantity] = useState('');
+  const [Day, setDay] = useState('');
+  const [Meal, setMeal] = useState('Breakfast');
+
+  const handleQuantityChange = (text) => {
+    setQuantity(text);
+  };
+
+  const handleDayChange = (text) => {
+    setDay(text);
+  };
+
+  const handleMealChange = (value) => {
+    setMeal(value);
+  };
 
   const handleSearch = async () => {
-    const appId = 'YOUR_APPLICATION_ID';
-    const appKey = 'YOUR_APPLICATION_KEY';
+    const appId = '2eb6fd30';
+    const appKey = '4529435b5d08170effb7fa9cb5db51c0';
 
     try {
       const response = await axios.get(
         `https://api.edamam.com/api/food-database/v2/parser?ingr=${query}&app_id=${appId}&app_key=${appKey}`
       );
       setSearchResults(response.data.hints);
+      if (response.data.hints.length === 0) {
+        setError('No food matching the query was found');
+      } else {
+        setError(null);
+      }
     } catch (error) {
+      setError('Error. Please try again.');
       console.error(error);
     }
   };
 
   return (
     <View style={styles.container}>
-      <TextInput
-        style={styles.searchInput}
-        placeholder="Search food..."
-        onChangeText={setQuery}
-      />
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}>
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <TextInput
+              style={styles.input}
+              placeholder="Quantity"
+              onChangeText={handleQuantityChange}
+              value={Quantity}
+            />
+            <TextInput
+              style={styles.input}
+              placeholder="Day"
+              onChangeText={handleDayChange}
+              value={Day}
+            />
+            <Text style={styles.label}>Select meal :</Text>
+            <Picker selectedValue={Meal} style={styles.picker} onValueChange={handleMealChange}>
+              <Picker.Item label="Breakfast" value="breakfast" />
+              <Picker.Item label="Lunch" value="lunch" />
+              <Picker.Item label="Snack" value="snack" />
+              <Picker.Item label="Dinner" value="dinner" />
+            </Picker>
+            <Button title="Update" onPress={() => setModalVisible(!modalVisible)}/>
+          </View>
+        </View>
+      </Modal>
+      <TextInput style={styles.searchInput} placeholder="Search food..." onChangeText={setQuery} />
       <Button title="Search" onPress={handleSearch} />
+      {error ? <Text style={styles.errorText}>{error}</Text> : null}
       <FlatList
         data={searchResults}
-        renderItem={({ item }) => <Text>{item.food.label}</Text>}
+        renderItem={({ item }) => (
+          <TouchableOpacity onPress={() => setModalVisible(true)}>
+            <Text style={styles.item}>
+              {item.food.label}
+              {'\n'}Cal:
+              {item.food.nutrients.ENERC_KCAL}
+            </Text>
+          </TouchableOpacity>
+        )}
         keyExtractor={(item) => item.food.foodId}
       />
     </View>
@@ -42,12 +113,60 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 16,
   },
+  textStyle: {
+    textAlign: 'center'
+  },
   searchInput: {
     height: 40,
     borderColor: 'gray',
     borderWidth: 1,
     marginBottom: 8,
     paddingHorizontal: 8,
+  },
+  errorText: {
+    color: 'red',
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    margin: 50,
+  },
+  modalView: {
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 50,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingHorizontal: 10,
+  },
+  label: {
+    marginTop: 10,
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  picker: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10,
+  },
+  item: {
+    backgroundColor: 'white',
+    padding: 20,
+    borderRadius: 20,
+    margin: 5
   },
 });
 
